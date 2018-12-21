3a763a5b12856c36fc0cc758de2caf8777edc0fb
Cleo Aguiar <cleotaisas@gmail.com>
2018-12-21 21:17:18 -0200

    fix: checkpoint training and saving model

--- Image Classifier Project/Image Classifier Project.py
+++ Image Classifier Project/Image Classifier Project.py
@@ -20,29 +20,31 @@ from torch import nn
 import torch.optim as optim
 
 
-data_dir = '/flower_data'
+# image = mpimg.imread('flower_data/train/1/image_06734.jpg')
+# plt.imshow(image
+
+data_dir = './flower_data'
 train_dir = data_dir + '/train'
 valid_dir = data_dir + '/valid'
 
 # TODO: Define your transforms for the training and validation sets
-data_transforms = transforms.Compose([
-    transforms.RandomRotation(30),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.CenterCrop(10),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+data_transforms = transforms.Compose([transforms.RandomResizedCrop(224),
+#                                       transforms.RandomRotation(30),
+                                      transforms.RandomHorizontalFlip(p=0.5),
+                                     transforms.CenterCrop(32),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
 
 # TODO: Load the datasets with ImageFolder
-train_data = datasets.ImageFolder(train_dir, transform=data_transforms)
-test_data = datasets.ImageFolder(valid_dir, transform=data_transforms)
+train_data = datasets.ImageFolder(train_dir,transform=data_transforms)
+test_data = datasets.ImageFolder(valid_dir,transform=data_transforms)
 
 # TODO: Using the image datasets and the trainforms, define the dataloaders
 
 # number of subprocesses to use for data loading
 num_workers = 0
 # how many samples per batch to load
-batch_size = 30
+batch_size = 980
 # percentage of training set to use as validation
 valid_size = 0.2
 
@@ -62,6 +64,11 @@ train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sa
 valid_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, sampler=valid_sampler, num_workers=num_workers)
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
 
+# print(valid_loader.dataset)
+# helper function to un-normalize and display an image
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    plt.imshow(np.transpose(img, (1, 2, 0)))  # convert from Tensor image
 
 import json
 
@@ -73,34 +80,92 @@ for key in cat_to_name:
     classes.append(cat_to_name[key])
 # classes
 
+# obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy() # convert images to numpy for display
+
+# plot the images in the batch, along with the corresponding labels
+fig = plt.figure(figsize=(25, 4))
+# display 20 images
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title(classes[labels[idx]])
+
 # TODO: Build and train your network
 model = models.vgg16(pretrained = True)
 # freeze all VGG parameters since we're only optimizing the target image
 for param in model.parameters():
     param.requires_grad_(False)
 
+# Create CNN model
+import torch.nn as nn
+import torch.nn.functional as F
+
+# define the CNN architecture
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        # convolutional layer (sees 32x32x3 image tensor)
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        # convolutional layer (sees 16x16x16 tensor)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        # convolutional layer (sees 8x8x32 tensor)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        # max pooling layer
+        self.pool = nn.MaxPool2d(2, 2)
+        # linear layer (1024 -> 500)
+        self.fc1 = nn.Linear(64 * 4 * 4, 500)
+        # linear layer (500 -> 10)
+        self.fc2 = nn.Linear(500, 10)
+        # dropout layer (p=0.25)
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        # add sequence of convolutional and max pooling layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        # flatten image input
+        x = x.view(-1, 64 * 4 * 4)
+        
+        # add dropout layer
+        x = self.dropout(x)
+        # add 1st hidden layer, with relu activation function
+        x = F.relu(self.fc1(x))
+        # add dropout layer
+        x = self.dropout(x)
+        # add 2nd hidden layer, with relu activation function
+        x = self.fc2(x)
+        return x
+
+# create a complete CNN
+model = Net()
+print(model)
+
 # Create classifier using Sequential with OrderedDict
-classifier = nn.Sequential(OrderedDict([
-                            ('conv1', nn.Conv2d(3, 16, 3, padding=1)),
-                            ('relu', nn.ReLU()),
-                            ('pool', nn.MaxPool2d(2, 2)),
-                            ('conv2', nn.Conv2d(16, 32, 3, padding=1)),
-                            ('relu', nn.ReLU()),
-                            ('pool', nn.MaxPool2d(2, 2)),
-                            ('conv3', nn.Conv2d(32, 64, 3, padding=1)),
-                            ('relu', nn.ReLU()),
-                            ('pool', nn.MaxPool2d(2, 2)),
+# classifier = nn.Sequential(OrderedDict([
+#                             ('conv1', nn.Conv2d(3, 16, 3, padding=1)),
+#                             ('relu', nn.ReLU()),
+#                             ('pool', nn.MaxPool2d(2, 2)),
+#                             ('conv2', nn.Conv2d(16, 32, 3, padding=1)),
+#                             ('relu', nn.ReLU()),
+#                             ('pool', nn.MaxPool2d(2, 2)),
+#                             ('conv3', nn.Conv2d(32, 64, 3, padding=1)),
+#                             ('relu', nn.ReLU()),
+#                             ('pool', nn.MaxPool2d(2, 2)),
 
-                            ('dropout', nn.Dropout(0.25)),
-                            ('fc1', nn.Linear(1024, 512)),
-                            ('relu', nn.ReLU()),
-                            ('dropout', nn.Dropout(0.25)),
-                            ('fc2', nn.Linear(512, 102)),
+#                             ('dropout', nn.Dropout(0.25)),
+#                             ('fc1', nn.Linear(1024, 512)),
+#                             ('relu', nn.ReLU()),
+#                             ('dropout', nn.Dropout(0.25)),
+#                             ('fc2', nn.Linear(512, 102)),
 
-                            ('output', nn.LogSoftmax(dim=1))
-                             ]))
+#                             ('output', nn.LogSoftmax(dim=1))
+#                              ]))
 
-model.classifier = classifier
+# model.classifier = classifier
 
 # specify loss function (categorical cross-entropy)
 criterion = nn.CrossEntropyLoss()
@@ -119,16 +184,21 @@ else:
 device = torch.device("cuda" if train_on_gpu else "cpu")
 model.to(device)
 
-# Train the Network
+## Train the Network
 # number of epochs to train the model
 n_epochs = 30
 
+valid_loss_min = np.Inf # track change in validation loss
+
 for epoch in range(1, n_epochs+1):
+
     # keep track of training and validation loss
     train_loss = 0.0
     valid_loss = 0.0
-
-    # Train the model
+    
+    ###################
+    # train the model #
+    ###################
     model.train()
     for data, target in train_loader:
         # move tensors to GPU if CUDA is available
@@ -137,7 +207,6 @@ for epoch in range(1, n_epochs+1):
         # clear the gradients of all optimized variables
         optimizer.zero_grad()
         # forward pass: compute predicted outputs by passing inputs to the model
-        # output = model.forward(data)
         output = model(data)
         # calculate the batch loss
         loss = criterion(output, target)
@@ -147,8 +216,10 @@ for epoch in range(1, n_epochs+1):
         optimizer.step()
         # update training loss
         train_loss += loss.item()*data.size(0)
-
-    # Validate the model
+        
+    ######################    
+    # validate the model #
+    ######################
     model.eval()
     for data, target in valid_loader:
         # move tensors to GPU if CUDA is available
@@ -160,15 +231,15 @@ for epoch in range(1, n_epochs+1):
         loss = criterion(output, target)
         # update average validation loss 
         valid_loss += loss.item()*data.size(0)
-
+    
     # calculate average losses
     train_loss = train_loss/len(train_loader.dataset)
     valid_loss = valid_loss/len(valid_loader.dataset)
-
+        
     # print training/validation statistics 
     print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
         epoch, train_loss, valid_loss))
-
+    
     # save model if validation loss has decreased
     if valid_loss <= valid_loss_min:
         print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
@@ -176,7 +247,6 @@ for epoch in range(1, n_epochs+1):
         valid_loss))
         torch.save(model.state_dict(), 'model_cifar.pt')
         valid_loss_min = valid_loss
-
 
 # TODO: Save the checkpoint
 
